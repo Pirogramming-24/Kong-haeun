3570281b92afcfb7b22621f42b919826f8dc3362
haeuniea <haeunhaa@gmail.com>
2026-01-22 11:45:29 +0900

    [feat] posts 모델 추가

--- piro24_Pirostagram/posts/models.py
+++ piro24_Pirostagram/posts/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Post(models.Model): # 작성자, 이미지, 내용, 작성 시간
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='posts'
+    )
+    image = models.ImageField(upload_to='posts/')
+    content = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
